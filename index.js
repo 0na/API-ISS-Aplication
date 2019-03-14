32814d9896f02b6edab088a769080d5a42aad085
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-03-14 21:07:29 +0100

    changes, code does not work

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Aplikacja ISS
+      </div>
+    );
+  }
+
+}
+
+ReactDOM.render(< App />, document.getElementById('application'));
